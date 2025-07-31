f5d527f878d8810268ea8b2b361ee628840544a9
Besman1985 <besman1985@gmail.com>
2025-07-31 21:48:50 +0300

    Next Commit

--- js/script.js
+++ js/script.js
@@ -26,3 +26,15 @@ personalMovieDB.movies[theFilms2] = grade2;
  console.log (personalMovieDB);
 
 
+const month = 9;
+
+
+if (month == 6 || month == 7 || month == 8) {
+  console.log ("Лето")
+} else if (month == 1 || month == 2 || month == 12) {
+  console.log ("Зима")
+} else if (month == 3 || month == 4 || month == 5) {
+  console.log ("Весна")
+} else if (month == 9 || month == 10 || month == 11) {
+  console.log ("Осень")
+};
