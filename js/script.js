403a2aabe38ac4d6894bceee7029e2dc8ae91463
BesmanWork <besman1985@gmail.com>
2025-08-01 16:24:06 +0300

    Commit 3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 "use strict";
 
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели",""),
-    theFilms = prompt("Один из последних просмотренных фильмов?",""),
-    grade = prompt("Насколько оцените его?",""),
-    theFilms2 = prompt("Один из последних просмотренных фильмов?",""),
-    grade2 = prompt("Насколько оцените его?","");
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели","");
 
 
-
+    
 let personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -16,8 +12,14 @@ let personalMovieDB = {
   genres: [],
   privat: false
 };
-personalMovieDB.movies[theFilms] = grade;
-personalMovieDB.movies[theFilms2] = grade2;
+
+for (let i = 0; i < 2; i++ ) {
+  let a = prompt("Один из последних просмотренных фильмов?",""),
+      b = prompt("Насколько оцените его?","");
+           personalMovieDB.movies[a] = b;
+};
+
+
 
 
 
