ad2500240827e403949295fe39c7391f3abf3371
BesmanWork <besman1985@gmail.com>
2025-08-01 17:52:50 +0300

    Commit 4

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели","");
-
-
     
 let personalMovieDB = {
   count: numberOfFilms,
@@ -18,13 +16,17 @@ for (let i = 0; i < 2; i++ ) {
       b = prompt("Насколько оцените его?","");
            personalMovieDB.movies[a] = b;
 };
+console.log (personalMovieDB);
 
 
+ let nam = 11;
+ (nam === 5) ? console.log("ok"): console.log("No");
 
 
-
-
-
- console.log (personalMovieDB);
-
-
+ if (nam < 5) {
+  console.log("ok")
+ } else if (nam < 10){
+  console.log("Normal")
+ } else {
+console.log("No")
+ };
